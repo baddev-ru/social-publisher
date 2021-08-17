2b8c96e60d3f081b77bd0e222ccea26066b5410c
artem_krutov <artem.krutov@genesys.com>
2021-08-17 18:22:25 +0400

    Initial commit

---
+++ src/main/java/ru/baddev/socialpublisher/SocialpublisherApplication.java
@@ -0,0 +1,13 @@
+package ru.baddev.socialpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/baddev/socialpublisher/SocialpublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.baddev.socialpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
